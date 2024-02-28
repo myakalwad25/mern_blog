f58afdc30edde48919015a20d276afe793b26694
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-02-28 13:08:36 +0530

    create and add sing in api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,9 @@
  import User from "../models/user.model.js";
  import bcryptjs from 'bcryptjs'
-import { errorHander } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
+import jwt  from "jsonwebtoken";
  
-const signup = async(req,res,next)=>{
+export const signup = async(req,res,next)=>{
     const{username, email, password }=req.body;
     if(!username || !email || !password || username==="" || email ==="" ||password ==="") {
         next(errorHandler(400,"all fields are required"));
@@ -25,6 +26,41 @@ const signup = async(req,res,next)=>{
         // console.log(err);
     }
 }  
+
+
+export const signin = async (req,res,next)=>{
+    const{ email, password }=req.body;
+    if(!email || !password  || email ==="" ||password ==="") {
+        next(errorHandler(400,"all fields are required"));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) {
+            return next(errorHandler(404,'Uesr not found'));
+
+        }
+        const validPassword =  bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) {
+           return  next(errorHandler(404,'Invalid password'));
+        }
+        const token = jwt.sign(
+            { id : validUser._id},process.env.JWT_SECRET);
+        
+
+        const {password:pass, ...rest} = validUser._doc;
+        res.status(200)
+            .cookie('access_token', token, {
+                httpOnly:true,
+            })
+            .json(rest);
+    }
+    catch(error) {
+        next(error)
+    }
+
+
+}
  
  
-export default signup; 
+ 
