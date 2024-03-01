9b14b67331c789687ddfe89b0418a307874185db
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-03-01 13:40:16 +0530

    added darkmode funtionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,13 +4,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link , useLocation} from "react-router-dom";
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-import { useSelector } from "react-redux";
-import { current } from "@reduxjs/toolkit";
+import {FaMoon, FaSun } from 'react-icons/fa'
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const {currentUser} = useSelector(state => state.user)
+  const {theme } = useSelector((state)=> state.theme);
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
@@ -30,8 +32,10 @@ export default function Header() {
         <AiOutlineSearch/>
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon/>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill
+        onClick={()=> dispatch(toggleTheme())}>
+          { theme === 'light' ? <FaSun/>:<FaMoon/>}
+          
         </Button>
         { currentUser ? (
           <Dropdown
@@ -46,7 +50,7 @@ export default function Header() {
           >
             <Dropdown.Header>
               <span className="block text-sm">@{currentUser.username}</span>
-              <span className="block text-sm fornt-medium truncate">
+              <span className="block text-sm font-medium truncate">
                 {currentUser.email}
               </span>
             </Dropdown.Header>
