a4740296bc1a39e8690da038bd10ead61dec7acc
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-03-04 03:16:19 +0530

    complete sidebar of dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,8 +19,7 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route element={<PrivateRoute/>}>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Route>
       <Route path="/projects" element={<Projects/>}/>
     </Routes>
