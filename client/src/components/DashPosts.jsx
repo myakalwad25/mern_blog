58ce97e7dba7447df63c8c1d6af97de6ab6d2818
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-03-09 13:32:39 +0530

    added showmore button and functionality in dashposts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
 
   console.log(userPosts);
 
@@ -16,6 +17,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -26,6 +30,27 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+
+  const handleShowMore = async ()=>{
+    const startIndex = userPosts.length;
+
+    try{
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok) {
+        setUserPosts((prev)=>[
+          ...prev, ...data.posts
+        ])
+        if(data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    }
+    catch(error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar
@@ -87,6 +112,11 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+              <button className="w-full text-teal-500 self-center text-sm py-7" onClick={handleShowMore}> Show more</button>
+            )
+          }
         </>
       ) : (
         <>
