0281a726bcc7f7a915853af78bdde467feb42d04
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-03-10 14:44:30 +0530

    added call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import {  useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -58,6 +59,10 @@ export default function PostPage() {
         <div className='p-3 max-w-2xl mx-auto w-full post-content'
          dangerouslySetInnerHTML={{__html:post && post.content}}>
 
+        </div>
+
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction/>
         </div>
 
     </main>
