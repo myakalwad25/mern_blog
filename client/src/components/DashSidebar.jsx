83b4025fe0cac024b44a691efba177b1dbaf4f07
Vijay Myakalwad <vijaymyakalwad25@gmail.com>
2024-03-14 02:28:06 +0530

    added comment section to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from "react-icons/hi";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -69,6 +69,17 @@ export default function DashSidebar() {
                 as="div"
               >
                 Users
+              </Sidebar.Item>
+            </Link>
+          )}
+           {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=comments">
+              <Sidebar.Item
+                active={tab === "comments"}
+                icon={HiAnnotation}
+                as="div"
+              >
+                Comments
               </Sidebar.Item>
             </Link>
           )}
--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -7,12 +7,13 @@ export const verifyToken = (req,res,next) =>{
     const token = req.cookies.access_token;
    
     if(!token) {
-       
+       console.log("here you go")
         return next(errorHandler(401, "Unauthorized"));
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err,user)=>{
         if(err) {
+            
             return next(errorHandler(401, "Unauthorized"));
  
         }
